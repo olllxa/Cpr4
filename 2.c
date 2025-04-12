73fdc2f79a42a838462f2772c07d56c666c07a8a
olllxa <oklashtorn@gmail.com>
2025-04-12 15:22:34 +0300

    Create 2.c

--- 2.c
+++ 2.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-int main() {
+int main() 
+{
   int blue_balls[5] = {1, 2, 3, 4, 5};
   int size = sizeof(blue_balls) / sizeof(blue_balls[0]);
   printf("В коробке blue_balls помещается %d шариков.\n", size);
